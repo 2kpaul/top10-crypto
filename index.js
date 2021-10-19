8fbf38d8948d4cef2d8ba6a50f8a871e23890fb5
Paul Maxineanu <paul.maxineanu@majoritas.com>
2021-10-19 13:07:42 +0300

    list top 10 crypto

---
+++ index.js
@@ -0,0 +1,50 @@
+const PORT = 8000
+const express = require('express')
+const cheerio = require('cheerio')
+const cheerioTableparser = require('cheerio-tableparser');
+const axios = require('axios')
+const path = require('path')
+
+const app = express()
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get('/', (req,res) => {
+    top = []
+    axios.get('https://www.coingecko.com/en')
+    .then((response) => {
+        const response_html = response.data
+        const $ = cheerio.load(response_html)
+        const top = []
+        let count = 0
+        //console.log($('div[class="coingecko-table"] table tbody tr').html())
+
+        $('div[class="coingecko-table"] table tbody tr').each((index, element) => {
+
+            const tds = $(element).find("td")
+
+            let name = $(tds[2]).find("div div.center a.tw-hidden").text()
+            let icon = $(tds[2]).find("div div.coin-icon img").attr('data-src')
+            let price = $(tds[3]).find("span").text()
+            let market_cap = $(tds[8]).find("span").text()
+            //console.log($(tds[8]).find("span").text())
+            if(count < 10) {
+                top.push({
+                    name,
+                    icon,
+                    price,
+                    market_cap
+                })
+            }
+            
+            count++
+        });
+
+        res.render('home', {top})
+        
+    })
+})
+
+
+app.listen(PORT, () => console.log('server running on port ' + PORT))
